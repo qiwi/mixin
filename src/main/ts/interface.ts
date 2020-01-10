bd127f3ee81ae4ee07c8594ae546f9e5498e2239
Anton Golub <mailbox@antongolub.ru>
2020-01-10 19:33:23 +0300

    chore: add basic interfaces and terms

---
+++ src/main/ts/interface.ts
@@ -0,0 +1,25 @@
+import {IAnyMap, UnionToIntersection} from '@qiwi/substrate'
+
+/**
+ * Something to be mixed into the target object
+ */
+export type IAdmixture<T=IAnyMap> = {
+  [P in keyof T]: T[P]
+}
+
+/**
+ * Function that extends the target object with several IAdmixtures.
+ *
+ * applier(target, admixture1, admixture12)
+ */
+export interface IApplier {
+  <T, U extends any[]>(target: T, ...mixins: U): T & UnionToIntersection<U[number]>
+}
+
+/**
+ * A function that somehow modifies the target object
+ */
+export interface IMixin<U extends any[]> {
+  <T extends {}>(target: T): T & UnionToIntersection<U[number]>
+}
+
