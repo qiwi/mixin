dc101b995aa86f23fe8a75d94d98e236c560dc9a
Anton Golub <antongolub@antongolub.com>
2021-11-10 19:23:19 +0300

    fix: repack esm/mjs entry
    closes #20

---
+++ src/test/ts/bundle.ts
@@ -0,0 +1,38 @@
+import { applyMixins } from '@qiwi/mixin'
+
+describe('bundle', () => {
+  it('works as in README example', () => {
+    interface IA {
+      a: () => string
+    }
+    interface IB {
+      b: () => string
+    }
+    class A implements IA {
+
+      constructor(param: number) {
+        console.log(param)
+      }
+
+      a() {
+        return 'a'
+      }
+
+    }
+    const b: IB = {
+      b() {
+        return 'b'
+      },
+    }
+
+    const c = applyMixins({}, A, b)
+    expect(c.a()).toBe('a') // c.a() // 'a'
+    expect(c.b()).toBe('b') // c.b() // 'b'
+
+    const D = applyMixins(A, b)
+    const d: InstanceType<typeof D> = new D(1)
+
+    expect(d.a()).toBe('a') // d.a() // 'a'
+    expect(d.b()).toBe('b') // d.b() // 'b'
+  })
+})
