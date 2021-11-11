a2f882a05e686b78c9f6bc3ea594fbedf74599fe
Anton Golub <antongolub@antongolub.com>
2021-11-11 12:31:43 +0300

    fix: rm type module, add intergation tests for esm, cjs and ts-node

---
+++ src/it/ts/index.ts
@@ -0,0 +1,29 @@
+import {applyMixins} from '@qiwi/mixin'
+import assert from 'assert'
+
+interface IA {
+  a: () => string
+}
+interface IB {
+  b: () => string
+}
+class A implements IA {
+  a() {
+    return 'a'
+  }
+}
+const b: IB = {
+  b() {
+    return 'b'
+  },
+}
+
+const c = applyMixins({}, A, b)
+assert(c.a() === 'a')
+assert(c.b() === 'b')
+
+const D = applyMixins(A, b)
+const d = new D()
+
+assert(d.a() === 'a')
+assert(d.b() === 'b')
